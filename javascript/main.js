c22a2f729999e9d3d67dcb3761a982cff76fcd60
terrythomas <terry.thomas@gmail.com>
2020-10-06 21:04:15 -0400

    Tuesday Commit

--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,14 @@ for (var i = 0; i < taskList.length; i++) {
     false
   );
 }
+
+// Modal Javascript
+
+const showModalBtn = document.getElementById("btnAddTask");
+const cntrModalAddTask = document.getElementById("containerModalAddTask");
+const sectionMask = document.getElementById("mask");
+
+showModalBtn.addEventListener("click", function () {
+  cntrModalAddTask.classList.add("show");
+  sectionMask.classList.add("show");
+});
